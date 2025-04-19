238836fbf03a17e51233b1fa43cb44e8c86d08e8
Ismat <ismetsemedov@gmail.com>
2025-04-19 13:22:45 +0400

    Update TechProfiles.tsx

--- src/components/TechProfiles.tsx
+++ src/components/TechProfiles.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { FaGithub, FaKaggle, FaLaptopCode, FaCode } from 'react-icons/fa'
+import { FaGithub, FaKaggle } from 'react-icons/fa'
 import { SiHackerrank, SiLeetcode, SiHuggingface, SiMedium } from 'react-icons/si'
 
 const profilesData = [
