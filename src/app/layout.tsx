35cc8d75a0e0b65c3e9dada86a609d5b96d62a9e
Ismat <ismetsemedov@gmail.com>
2025-04-19 14:10:45 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Ismat Samadov | Data & Product Analyst',
-  description: 'Professional portfolio of Ismat Samadov, a Data & Product Analyst specializing in machine learning, predictive modeling, and full-stack development.',
+  title: 'Ismat Samadov | Analyst & Engineer',
+  description: 'Professional portfolio of Ismat Samadov, an Analyst & Engineer specializing in machine learning, predictive modeling, and full-stack development.',
 }
 
 export default function RootLayout({
